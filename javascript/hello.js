5b4b4798ce85d327eb6228f5dd0c5a0115981ad0
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2020-03-26 16:57:47 +0100

    for schleife ueber ein array

--- javascript/hello.js
+++ javascript/hello.js
@@ -79,3 +79,8 @@ console.log(a);
 for (let i=0; i<10; i++) {
     console.log(`Schleife ${i}`);
 }
+
+for (let j = 0; j < participants.length; j++) {
+    const participant = participants[j];
+    console.log(`Teilnehmer*in ${j} ${participant}`);
+}
