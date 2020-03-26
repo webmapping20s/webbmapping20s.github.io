1dc0a6a0d44e80da4bab4bba106fd04214704a44
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2020-03-26 16:05:40 +0100

    strings in javascript

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,2 +1,22 @@
 // alert("Hello world");
 
+// let message = "Hello world";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
+
+const LINK_COLOR = "#ff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = 'Smith';
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
+let template = `Dein Highscore sind ${highscore} Punkte`;
+console.log(template);
