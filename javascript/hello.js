315e896ffb41a66aa59bf04f034ce8f91c60ee94
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2020-03-26 16:26:42 +0100

    arrays/objects

--- javascript/hello.js
+++ javascript/hello.js
@@ -20,3 +20,37 @@ console.log(fullname);
 
 let template = `Dein Highscore sind ${highscore} Punkte`;
 console.log(template);
+
+let isOver18 = true;
+console.log(isOver18);
+
+let age = 19;
+console.log("über 18?", age > 18);
+
+let participants = [ "John", "Jane", "Max" ];
+console.log(participants);
+console.log("Einträge im Array: ", participants.length);
+console.log(participants[2]);
+
+let gameHighscores = [2099, 3010, 3333, 5000 ];
+console.log(gameHighscores);
+
+let user = {
+    firstname: "John",
+    lastname: "Smith",
+    age: 25
+};
+
+console.log(user);
+console.log(user.firstname);
+user.highscore = 200;
+user["highscore ever"] = 400;
+console.log(user);
+
+let a = 2;
+let b = 4;
+console.log(a+b);
+console.log(b/(a-1));
+a++;
+console.log(a);
+
