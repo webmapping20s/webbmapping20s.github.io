be5ae544f9f7c6dcf25c9dcb44f01ac5c5f6669d
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 15:58:03 +0200

    tileLayer verändert

--- nz/main.js
+++ nz/main.js
@@ -5,11 +5,11 @@ let map = L.map("map", {
         mapdiv.dataset.lat,
         mapdiv.dataset.lng
     ],
-    zoom: 13,
+    zoom: 17,
     layers: [
         L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
             maxZoom: 17,
-            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+            attribution: `Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`
         })
     ]
 });
