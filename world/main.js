45826e1c5f53ce5af60fbf7561fb7ec1ff6d31fc
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 16:49:36 +0200

    weiter Hintergrundlayer hinzufügen

--- world/main.js
+++ world/main.js
@@ -1,5 +1,5 @@
 
-let startLayer = L.tileLayer.provider("OpenTopoMap");
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
 
 let map = L.map("map", {
     center: [0,0],
@@ -10,8 +10,16 @@ let map = L.map("map", {
 });
 
 L.control.layers({
-    "OpenTopoMap" : startLayer,
+    "OpenTopoMap" : L.tileLayer.provider("OpenTopoMap"),
     "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-
-
+    "Stamen.TonerLite" : L.tileLayer.provider("Stamen.TonerLite"),
+    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.Terrain" : L.tileLayer.provider("Stamen.Terrain"),
+    "Stamen.TerrainBackground" : L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+    "Esri.WorldTopoMap": startLayer,
+    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron"),
 }).addTo(map)
