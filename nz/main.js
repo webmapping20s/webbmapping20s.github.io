5719e076d699b9a8149ddd24797fe1065db0cb17
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 16:04:53 +0200

    leaflet-providers Plugin eingebaut

--- nz/main.js
+++ nz/main.js
@@ -7,10 +7,7 @@ let map = L.map("map", {
     ],
     zoom: 17,
     layers: [
-        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            maxZoom: 17,
-            attribution: `Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`
-        })
+        L.tileLayer.provider("OpenTopoMap")
     ]
 });
 
