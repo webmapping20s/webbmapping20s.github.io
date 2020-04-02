bdbb618e5ca88009352c2c7e3bd1636a6585826d
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 16:36:38 +0200

    Layer control hinzugefügt

--- world/main.js
+++ world/main.js
@@ -1,7 +1,17 @@
+
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+
 let map = L.map("map", {
     center: [0,0],
     zoom: 2,
     layers: [
-        L.tileLayer.provider("OpenTopoMap")
+        startLayer
     ]
 });
+
+L.control.layers({
+    "OpenTopoMap" : startLayer,
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+
+
+}).addTo(map)
