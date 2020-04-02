cb98c6a3a175bb19b8e4cf4df1086753879f776c
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 18:08:51 +0200

    beautify script

--- world/main.js
+++ world/main.js
@@ -1,8 +1,7 @@
-
 let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
 
 let map = L.map("map", {
-    center: [0,0],
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
@@ -12,40 +11,44 @@ let map = L.map("map", {
 let circleGroup = L.featureGroup().addTo(map);
 
 L.control.layers({
-    "OpenTopoMap" : L.tileLayer.provider("OpenTopoMap"),
-    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Stamen.TonerLite" : L.tileLayer.provider("Stamen.TonerLite"),
-    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
-    "Stamen.Terrain" : L.tileLayer.provider("Stamen.Terrain"),
-    "Stamen.TerrainBackground" : L.tileLayer.provider("Stamen.TerrainBackground"),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Stamen.TonerLite": L.tileLayer.provider("Stamen.TonerLite"),
+    "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
+    "Stamen.TerrainBackground": L.tileLayer.provider("Stamen.TerrainBackground"),
     "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
     "Esri.WorldTopoMap": startLayer,
     "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
     "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
     "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
     "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
-},{
-    "Thematische Darstellung" : circleGroup
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-//console.log(CONFIRMED);
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    //console.log(row[2],row[3]);
-    let reg = `${row[0]} ${row[1]}`;
-    let lat = row[2];
-    let lng = row[3];
-    let val = row[row.length-1];
-    //let mrk = L.marker([lat,lng]).addTo(map);
-    //mrk.bindPopup(`${reg}: ${val}`);
+let drawCircles = function (data) {
+    //console.log(data);
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length - 1];
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
-    //A = r²*PI
-    //r² = A/PI
-    //r = WURZEL(A/PI)
-    let s = 0.5;
-    let r = Math.sqrt(val*s/Math.PI);
-    let circle = L.circleMarker([lat,lng],{
-        radius: r
-    }).addTo(circleGroup);
-    circle.bindPopup(`${reg}: ${val}`);
-}
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
+        let s = 0.5;
+        let r = Math.sqrt(val * s / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+};
+
+drawCircles(CONFIRMED);
