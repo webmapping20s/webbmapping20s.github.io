2c7d6cc5c054359127c48252420ab6caf6f075d6
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-02 15:46:10 +0200

    kartenhintergrund gesetzt

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,15 @@
-//alert("Hallo Welt!")
+let mapdiv = document.querySelector("#map");
+
+let map = L.map("map", {
+    center: [
+        mapdiv.dataset.lat,
+        mapdiv.dataset.lng
+    ],
+    zoom: 13,
+    layers: [
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        })
+    ]
+});
