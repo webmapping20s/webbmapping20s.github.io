5ff46eee4f519899834f4d77da52f920fdd2fc3e
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2020-04-16 16:37:53 +0200

    add featuregroup and zoomTo extend of group

--- wien/main.js
+++ wien/main.js
@@ -8,6 +8,8 @@ let map = L.map("map", {
     ]
 });
 
+let walkGroup = L.featureGroup().addTo(map);
+
 L.control.layers({
     "BasemapAT.grau": startLayer,
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,6 +22,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Stadtspaziergang (Punkte)": walkGroup
 }).addTo(map);
 
 let walkUrl = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD &srsName=EPSG:4326&outputFormat=json";
@@ -39,4 +43,9 @@ let walk = L.geoJson.ajax(walkUrl, {
         `);
         return marker;
     }
-}).addTo(map);
+}).addTo(walkGroup);
+
+walk.on("data:loaded", function() {
+  console.log('data loaded!');
+  map.fitBounds(walkGroup.getBounds());
+});
