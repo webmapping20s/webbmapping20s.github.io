4a0a94d7a8f26efb9686b4157ead8374a03fe952
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2020-04-16 16:14:26 +0200

    marker icon hinzugefügt

--- wien/main.js
+++ wien/main.js
@@ -24,7 +24,13 @@ L.control.layers({
 
 let walk = L.geoJson(SPAZIERGANG, {
     pointToLayer: function(point, latlng) {
-        let marker = L.marker(latlng);
+        let icon = L.icon({
+            iconUrl: 'icons/sight.svg',
+            iconSize: [32, 32]
+        });
+        let marker = L.marker(latlng, {
+            icon: icon
+        });
         console.log("Point", point);
         marker.bindPopup(`<h3>${point.properties.NAME}</h3>
         <p><a target="links" href="${point.properties.WEITERE_INF}">Link</a></p>
