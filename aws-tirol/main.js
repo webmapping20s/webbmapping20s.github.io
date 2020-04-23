b80276abe9318bcf90a890d695303de130b516c8
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-23 15:53:26 +0200

    aws.on("data:loaded") implementiert

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,8 +1,6 @@
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    center: [47.3, 11.5],
-    zoom: 8,
     layers: [
         startLayer
     ]
@@ -53,3 +51,11 @@ let aws = L.geoJson.ajax(awsUrl, {
         return marker;
     }
 }).addTo(overlay.stations);
+
+aws.on("data:loaded", function() {
+    console.log(aws.toGeoJSON());
+
+    map.fitBounds(overlay.stations.getBounds());
+
+    overlay.stations.addTo(map);
+});
