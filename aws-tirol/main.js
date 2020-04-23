db722a0d4cd36c8f3888db1bea96a99190d82982
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-23 18:09:00 +0200

    Pfeile für Windrichtung mit Font Awesome

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -106,11 +106,11 @@ let drawWind = function(jsonData) {
         },
         pointToLayer: function(feature, latlng) {
             let kmh = Math.round(feature.properties.WG / 1000 * 3600);
-            let color = getColor(feature.properties.WG,COLORS.wind);
+            let color = getColor(kmh,COLORS.wind);
             return L.marker(latlng, {
-                title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
+                title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m) - ${kmh} km/h`,
                 icon: L.divIcon({
-                    html: `<div class="label-wind" style="background-color:${color}">${kmh}</div>`,
+                    html: `<div class="label-wind"><i class="fas fa-arrow-circle-up" style="color:${color}"></i></div>`,
                     className: "ignore-me" // dirty hack
                 })
             })
