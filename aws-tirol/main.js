03e8dc8962d1da843327b596f287258c4e84af08
Klaus Förster <openwebcc@github.com>
2020-04-24 12:28:50 +0200

    ° bei Temperaturwerten dazu

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -89,7 +89,7 @@ let drawTemperature = function(jsonData) {
             return L.marker(latlng, {
                 title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`,
                 icon: L.divIcon({
-                    html: `<div class="label-temperature" style="background-color:${color}">${feature.properties.LT.toFixed(1)}</div>`,
+                    html: `<div class="label-temperature" style="background-color:${color}">${feature.properties.LT.toFixed(1)}°</div>`,
                     className: "ignore-me" // dirty hack
                 })
             })
