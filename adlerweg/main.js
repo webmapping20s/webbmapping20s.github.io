2e4cfa6e46d0a3a2df1b0cf45f806e6810a8676b
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-30 18:51:13 +0200

    mit Etappe 1 starten

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -85,8 +85,9 @@ let drawEtappe = function(nr) {
             console.log(val);
         }
     }
+
 };
-drawEtappe(10);
+drawEtappe(1);
 
 let pulldown = document.querySelector("#pulldown");
 //console.log(pulldown);
