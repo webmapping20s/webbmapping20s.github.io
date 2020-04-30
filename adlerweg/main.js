bec7398b26746e4b0dcf7af6310ff5c5942d2550
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-30 17:34:37 +0200

    Etappen in Overlay

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -9,7 +9,8 @@ let map = L.map("map", {
 });
 
 let overlay = {
-    adlerblicke: L.featureGroup()
+    adlerblicke: L.featureGroup(),
+    etappen: L.featureGroup()
 };
 
 L.control.layers({
@@ -25,7 +26,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Adlerblicke": overlay.adlerblicke
+    "Adlerblicke": overlay.adlerblicke,
+    "Adlerweg Etappen": overlay.etappen
 }).addTo(map);
 
 //console.log(ETAPPEN);
@@ -64,4 +66,5 @@ let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
 
 gpx.on("loaded", function(evt) {
     map.fitBounds(evt.target.getBounds());
-}).addTo(map);
+}).addTo(overlay.etappen);
+overlay.etappen.addTo(map);
