2a7c75b4d6e0d24a4fa55fa8b9df5f71a55d0d5c
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-04-30 16:04:33 +0200

    template Adlerweg

---
+++ adlerweg/main.js
@@ -0,0 +1,23 @@
+let startLayer = L.tileLayer.provider("BasemapAT.terrain");
+
+let map = L.map("map", {
+    center: [47.25, 11.5],
+    zoom: 9,
+    layers: [
+        startLayer
+    ]
+});
+
+L.control.layers({
+    "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT.terrain": startLayer,
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+}).addTo(map);
