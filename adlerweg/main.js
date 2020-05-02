24aa6fb4d0ba9d68a62562597f38da63537bbdc2
Klaus Förster <openwebcc@github.com>
2020-05-02 18:33:25 +0200

    console.log auskommentieren

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -79,11 +79,11 @@ let drawEtappe = function(nr) {
     for (const key in ETAPPEN[nr]) {
         if (ETAPPEN[nr].hasOwnProperty(key)) {
             const val = ETAPPEN[nr][key];
-            console.log(`et-${key}`);
+            //console.log(`et-${key}`);
             let elem = document.querySelector(`#et-${key}`);
             if (elem) {
                 elem.innerHTML = val;
-                console.log(val);
+                //console.log(val);
             }
         }
     }
