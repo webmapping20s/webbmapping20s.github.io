80b5b458e61dcc723e2318014111f57b42155b30
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-05-07 16:20:56 +0200

    Profil implementiert

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -75,6 +75,8 @@ let drawEtappe = function (nr) {
 
     gpx.on("loaded", function (evt) {
         map.fitBounds(evt.target.getBounds());
+        controlElevation.clear();
+        controlElevation.load(`gpx/AdlerwegEtappe${track}.gpx`);
     }).addTo(overlay.etappen);
     overlay.etappen.addTo(map);
 
@@ -131,3 +133,10 @@ let drawEinkehr = function () {
 };
 drawEinkehr();
 overlay.einkehr.addTo(map);
+
+let controlElevation = L.control.elevation({
+    theme: "steelblue-theme",
+    detached: true,
+    elevationDiv: "#profile",
+    followMarker: false
+}).addTo(map);
