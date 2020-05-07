e6990132fa251db2c26cee99d6252eb14a722268
klaus.foerster@uibk.ac.at <klaus.foerster@uibk.ac.at>
2020-05-07 15:52:24 +0200

    Einkehrmöglichkeiten als Marker mit Popup

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -10,7 +10,8 @@ let map = L.map("map", {
 
 let overlay = {
     adlerblicke: L.featureGroup(),
-    etappen: L.featureGroup()
+    etappen: L.featureGroup(),
+    einkehr: L.featureGroup()
 };
 
 L.control.layers({
@@ -27,7 +28,8 @@ L.control.layers({
     ])
 }, {
     "Adlerblicke": overlay.adlerblicke,
-    "Adlerweg Etappen": overlay.etappen
+    "Adlerweg Etappen": overlay.etappen,
+    "Einkehrmöglichkeiten": overlay.einkehr
 }).addTo(map);
 
 //console.log(ETAPPEN);
@@ -111,3 +113,14 @@ pulldown.onchange = function (evt) {
     //console.log(nr);
     drawEtappe(nr);
 }
+
+
+let drawEinkehr = function () {
+    for (let einkehr of EINKEHR) {
+        //console.log(einkehr);
+        let mrk = L.marker([einkehr[2],einkehr[3]]).addTo(overlay.einkehr);
+        mrk.bindPopup(`${einkehr[1]} (Etappe ${einkehr[0]})`);
+    }
+};
+drawEinkehr();
+overlay.einkehr.addTo(map);
