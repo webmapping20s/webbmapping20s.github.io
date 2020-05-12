9630e2c64786b9a587c32f9dc63fd4ad98f0b8b0
Klaus Förster <openwebcc@github.com>
2020-05-12 14:28:04 +0200

    überflüssiges center und zoom auskommentieren

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -1,8 +1,8 @@
 let startLayer = L.tileLayer.provider("BasemapAT.terrain");
 
 let map = L.map("map", {
-    center: [47.25, 11.5],
-    zoom: 9,
+    //center: [47.25, 11.5],
+    //zoom: 9,
     layers: [
         startLayer
     ]
